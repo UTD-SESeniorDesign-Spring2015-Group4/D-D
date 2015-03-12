e86fe91f9f2569936dc958aa5f75b326966d795e
Jonathan Darling <jmdarling@me.com>
2015-03-11 19:43:24 -0500

    Adds canvas and demo shapes.

---
+++ app/js/app.js
@@ -0,0 +1,40 @@
+(function () {
+  "use strict";
+
+  var $canvas;
+
+  var graph;
+  var paper;
+
+
+
+  $(document).ready(function() {
+    setUpCommonQueries();
+    setUpPaper();
+  });
+
+  function setUpCommonQueries() {
+    $canvas = $("#canvas");
+  }
+
+  function setUpPaper() {
+    graph = new joint.dia.Graph;
+    paper = new joint.dia.Paper({
+      el: $canvas,
+      width: $canvas.width(),
+      height: $canvas.height(),
+      gridSize: 10,
+      model: graph
+    });
+
+    for (var i = 0; i < 50; i++) {
+      var rect = new joint.shapes.basic.Rect({
+        position: { x: 50 + i * 10, y: 70 + i * 10 },
+        size: { width: 100, height: 40 }
+      });
+      graph.addCell(rect);
+    }
+  }
+
+
+})(jQuery);
