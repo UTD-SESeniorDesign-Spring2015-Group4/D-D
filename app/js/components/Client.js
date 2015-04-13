e775612bde769ceecb253f1f0addd6c6f2bf2232
Jonathan Darling <jmdarling@me.com>
2015-04-13 01:01:47 -0500

    Adds the ability to name a component by double clicking it. Abstracts functions common to all components to the component class.

--- app/js/components/Client.js
+++ app/js/components/Client.js
@@ -12,7 +12,7 @@ define([
             '<path id="laptop-icon" fill="#333333" d="M783.3-0.1H212v384.7h571.1V-0.1H783.3z M734.7,336.1H260.5V48.4h474.1v287.7H734.7zM783.3,411.7H212l-84.4,148.4h740.7L783.3,411.7z M429.2,528.6l14.4-42.2h108.7l14.4,42.2H429.2z"/>',
             '<rect x="127.6" y="-0.1" fill="rgba(0, 0, 0, 0)" width="740.7" height="560.2"/>',
           '</g>',
-          '<text class="name"><%= name %></text>'
+          '<text class="name" font-size="15" x="0" y="75"><%= name %></text>'
         ].join('')
       ),
       attrs: {
@@ -24,14 +24,6 @@ define([
         }
       },
       paletteIcon: 'img/client_palette.svg'
-    },
-    initialize: function() {
-      // Dynamically generate the markup using a template.
-      this.set('markup', this.get('template')(this.attributes));
-
-      // This overrides its super initialize function.
-      // To use this, must call the parent prototype's initialize function.
-      Component.prototype.initialize.apply(this, arguments);
     }
   });
 
--- app/js/components/Component.js
+++ app/js/components/Component.js
@@ -6,11 +6,23 @@ define([], function(){
       type: '',
       paletteIcon: '',
     },
-
     initialize: function() {
+      // Dynamically generate the markup using the component's template.
+      this.set('markup', this.get('template')(this.attributes));
+
+      // Set change listeners.
+      this.on('change:name', this.updateName, this);
+      this.on('change:markup', this.updateMarkup, this);
+
       // This overrides its super initialize function.
       // To use this, must call the parent prototype's initialize function.
       joint.shapes.basic.Generic.prototype.initialize.apply(this, arguments)
+    },
+    updateName: function() {
+      this.set('markup', this.get('template')(this.attributes));
+    },
+    updateMarkup: function() {
+      this.findView(paper).render();
     }
   });
 
