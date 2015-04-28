6f4be13b6b22fa96d7abcfba5c15f79d7185b73f
Jonathan Darling <jmdarling@me.com>
2015-04-28 12:16:02 -0500

    Fixes text alignment issues with SVGs.

--- app/js/components/ApplicationServer.js
+++ app/js/components/ApplicationServer.js
@@ -14,14 +14,12 @@ define([
 			// initialize() function.
 			template: _.template(
 				[
-					'<g>',
-					'<text class="type" font-size="15" x="0" y="0"><%= type %></text>',
+					'<text class="type" text-anchor="middle" font-size="15" x="47" y="0"><%= type %></text>',
 					'<g class="scalable">',
-					'<path d="M227.1,0v392.2h507.4V0H227.1z M682.7,340.3H279.2V51.8h403.5V340.3z M458.2,197.5L380.5,270l-28.1-26l50-46.5l-50.2-46.7l28.1-26L458.2,197.5z M596.1,232.7H479.9v36h116.3V232.7z M734.5,432.5h-509V560h509V432.5L734.5,432.5z M259.3,524.3l22.5-56h27.1l-22.5,56H259.3z M303.1,524.3l22.5-56h27.1l-22.5,56H303.1z M347,524.3l22.5-56h27.1l-22.5,56H347z M390.8,524.3l22.5-56h27.1l-22.5,56H390.8z M461.6,524.3h-27.1l22.5-56h27.1L461.6,524.3z M674,514.9c-10.3,0-18.6-8.4-18.6-18.6c0-10.3,8.4-18.6,18.6-18.6c10.3,0,18.6,8.4,18.6,18.6C692.7,506.6,684.3,514.9,674,514.9z"/>',
-					'<rect x="227.1" y="0" fill="rgba(0, 0, 0, 0)" width="507.4" height="560"/>', // Invisible rectangle to allow the component to be clicked where it should be blank.
+					'<path transform="translate(0, 50)" d="M227.1,0v392.2h507.4V0H227.1z M682.7,340.3H279.2V51.8h403.5V340.3z M458.2,197.5L380.5,270l-28.1-26l50-46.5l-50.2-46.7l28.1-26L458.2,197.5z M596.1,232.7H479.9v36h116.3V232.7z M734.5,432.5h-509V560h509V432.5L734.5,432.5z M259.3,524.3l22.5-56h27.1l-22.5,56H259.3z M303.1,524.3l22.5-56h27.1l-22.5,56H303.1z M347,524.3l22.5-56h27.1l-22.5,56H347z M390.8,524.3l22.5-56h27.1l-22.5,56H390.8z M461.6,524.3h-27.1l22.5-56h27.1L461.6,524.3z M674,514.9c-10.3,0-18.6-8.4-18.6-18.6c0-10.3,8.4-18.6,18.6-18.6c10.3,0,18.6,8.4,18.6,18.6C692.7,506.6,684.3,514.9,674,514.9z"/>',
+					'<rect transform="translate(0, 50)" x="227.1" y="0" fill="rgba(0, 0, 0, 0)" width="507.4" height="560"/>', // Invisible rectangle to allow the component to be clicked where it should be blank.
 					'</g>',
-					'<text class="name" font-size="15" x="0" y="75"><%= name %></text>',
-					'</g>'
+					'<text class="name" text-anchor="middle" font-size="15" x="47" y="77"><%= name %></text>'
 				].join('')
 			),
 
--- app/js/components/MiddlewareServer.js
+++ app/js/components/MiddlewareServer.js
@@ -14,12 +14,12 @@ define([
 			// initialize() function.
 			template: _.template(
 				[
-					'<text class="type" font-size="15" x="0" y="0"><%= type %></text>',
+					'<text class="type" text-anchor="middle" font-size="15" x="37" y="0"><%= type %></text>',
 					'<g class="scalable">',
-					'<path id="server-icon" fill="#333333" d="M533.2,420.3h-75.3v73h-56.5v0.9v65.7h188.3v-65.7v-0.9h-56.5V420.3L533.2,420.3zM759.9,493.3H627.3v66.6h132.5V493.3z M363.7,493.3H231.1v66.6h132.5V493.3z M838.6,203.7H152.4v172h686.1L838.6,203.7L838.6,203.7z M198.1,327.3l30.2-75.5h36.4l-30.2,75.5H198.1z M257.1,327.3l30.2-75.5h36.4l-30.2,75.5H257.1z M316.1,327.3l30.2-75.5h36.4l-30.2,75.5H316.1z M375.2,327.3l30.2-75.5h36.4l-30.2,75.5H375.2z M470.8,327.3h-36.4l30.2-75.5H501L470.8,327.3zM757.2,314.9c-14,0-25.2-11.4-25.2-25.2s11.4-25.2,25.2-25.2c14,0,25.2,11.4,25.2,25.2S771.1,314.9,757.2,314.9z M800.7,158.6h-609L311.8,0.1h367.4L800.7,158.6z"/>',
-					'<rect x="152.4" y="0.1" fill="rgba(0, 0, 0, 0)" width="686.1" height="559.8"/>', // Invisible rectangle to allow the component to be clicked where it should be blank.
+					'<path transform="translate(0, 50)" id="server-icon" fill="#333333" d="M533.2,420.3h-75.3v73h-56.5v0.9v65.7h188.3v-65.7v-0.9h-56.5V420.3L533.2,420.3zM759.9,493.3H627.3v66.6h132.5V493.3z M363.7,493.3H231.1v66.6h132.5V493.3z M838.6,203.7H152.4v172h686.1L838.6,203.7L838.6,203.7z M198.1,327.3l30.2-75.5h36.4l-30.2,75.5H198.1z M257.1,327.3l30.2-75.5h36.4l-30.2,75.5H257.1z M316.1,327.3l30.2-75.5h36.4l-30.2,75.5H316.1z M375.2,327.3l30.2-75.5h36.4l-30.2,75.5H375.2z M470.8,327.3h-36.4l30.2-75.5H501L470.8,327.3zM757.2,314.9c-14,0-25.2-11.4-25.2-25.2s11.4-25.2,25.2-25.2c14,0,25.2,11.4,25.2,25.2S771.1,314.9,757.2,314.9z M800.7,158.6h-609L311.8,0.1h367.4L800.7,158.6z"/>',
+					'<rect transform="translate(0, 50)" x="152.4" y="0.1" fill="rgba(0, 0, 0, 0)" width="686.1" height="559.8"/>', // Invisible rectangle to allow the component to be clicked where it should be blank.
 					'</g>',
-					'<text class="name" font-size="15" x="0" y="75"><%= name %></text>'
+					'<text class="name" text-anchor="middle" font-size="15" x="37" y="60"><%= name %></text>'
 				].join('')
 			),
 
